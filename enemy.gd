d81e14460564a7a3262b507f273ec67519fa3d90
csmoulaison <csmoulaison@gmail.com>
2026-03-02 21:22:45 -0800

    Start on utility AI with attractor system

--- enemy.gd
+++ enemy.gd
@@ -1,17 +1,62 @@
 extends CharacterBody3D
 
+enum BunnyChaseState { PATROL, DISTRACTED, INVESTIGATE, CHASE }
+
+# Bunny Chase Logic
+# Whenever an object which makes conspicuous noises makes a conspicuous noise,
+# it emits an "attract" signal, which is handled by our _on_attract function 
+# which keeps track of those signals.
+#
+# When an attraction is emitted, its position is checked against the existing
+# targets for whether it falls within a distance threshold. If it is within the
+# threshold, the score associated with the attractor is added to the matching
+# target and its position is updated to match the new attraction. If not, a new
+# target is created with the associated score.
+#
+# The target scores are constantly depreciating, so the most recent attractions
+# are more likely to be engaged with. Every tick, we see if any targets have a
+# high enough threshold to warrant a response, and our hare reacts accordingly.
+class Target:
+	var position: Vector3
+	var score: float
+
+@export var player: Node3D
+@export var path_nodes: Array[Node3D]
+@export var speed = 3.0
+@export var acceleration = 20.0
+@export var state: BunnyChaseState
+
 @onready var nav = $NavigationAgent3D
+@onready var current_node = 0
 
-@export var target: Node3D
-@export var speed = 3
+var targets: Array[Target]
 
-enum BunnyState { IDLE, CHASE }
+func _ready():
+	var attractors = get_tree().get_nodes_in_group("Attractors")
+	for attractor in attractors:
+		attractor.attract.connect(on_attract)
+	
+func on_attract(pos: Vector3, score: float):
+	# TODO(conner): Calculate actual score based on distance and combine with
+	# previous targets as needed. Maybe add a third parameter for the "staying
+	# power" of the attraction, which is either averaged with or overwrites the
+	# previous value of the target, and controls how slow the cooldown effect is.
+	print("Attract | pos: ", pos, " score: ", score)
+	
+func _process(dt: float):
+	# TODO(conner): Figure out current target/state based on currently tracked
+	# targets. Represent all possible actions as utility functions, and add a 
+	# bias towards recently switch-to states to prevent schizophrenia.
+	var k = 0 # temp to prevent error
 
-@export var state: BunnyState
-
-func _physics_process(delta: float):
-	nav.target_position = target.global_position
+func _physics_process(dt: float):
+	nav.target_position = path_nodes[current_node].position
 	var cur_position = global_transform.origin
 	var next_position = nav.get_next_path_position()
-	velocity = (next_position - cur_position).normalized() * speed
+	velocity = velocity.move_toward((next_position - cur_position).normalized() * speed, acceleration * dt)
 	move_and_slide()
+	
+	if nav.is_target_reached():
+		current_node += 1
+		if current_node >= len(path_nodes):
+			current_node = 0
--- player.gd
+++ player.gd
@@ -1,15 +1,20 @@
 extends CharacterBody3D
 
+signal attract(pos: Vector3, score: float)
+
 @export var speed = 10.0
+@export var acceleration = 50.0
 @export var fall_acceleration = 75.0
 @export var mouse_sensitivity = 0.01
+@export var footsteps_per_second = 0.5
 
 var target_velocity = Vector3.ZERO
+var footstep_cooldown = footsteps_per_second * 0.5
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _physics_process(dt):
+func _physics_process(dt: float):
 	var input  = Vector2.ZERO
 	
 	if Input.is_action_pressed("move_right"):
@@ -23,24 +28,33 @@ func _physics_process(dt):
 		
 	if input != Vector2.ZERO:
 		input = input.normalized()
+		footstep_cooldown -= dt
+		if(footstep_cooldown < 0.0):
+			footstep_cooldown = footsteps_per_second
+			$FootstepSound.play()
+			emit_signal("attract", position, 5.0)
+	else:
+		footstep_cooldown = footsteps_per_second * 0.5
+			
 	var direction = (transform.basis * Vector3(input.x, 0.0, input.y))
 		
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	target_velocity.x = move_toward(target_velocity.x, direction.x * speed, acceleration * dt)
+	target_velocity.z = move_toward(target_velocity.z, direction.z * speed, acceleration * dt)
 	
 	if not is_on_floor():
-		target_velocity.y = target_velocity.y - (fall_acceleration * dt)
+		velocity.y = velocity.y - (fall_acceleration * dt)
 		
-	velocity = target_velocity
+	velocity.x = target_velocity.x
+	velocity.z = target_velocity.z
 	move_and_slide()
 	
 func _input(event):
 	if event is InputEventMouseMotion:
 		$CameraPivot.rotate_object_local(Vector3.LEFT, event.relative.y * mouse_sensitivity)
-		rotate_object_local(Vector3.DOWN, event.relative.x * mouse_sensitivity)
+		rotate_object_local(Vector3.DOWN, event.relative.x * mouse_sensitivity)		
 			
-	if $CameraPivot.rotation.x < deg_to_rad(-89) or $CameraPivot.rotation.x > deg_to_rad(89):
-		$CameraPivot.rotation.x = clamp($CameraPivot.rotation.x, deg_to_rad(-89), deg_to_rad(89))
-	
-	orthonormalize()
-	$CameraPivot.orthonormalize()
+		if $CameraPivot.rotation.x < deg_to_rad(-89) or $CameraPivot.rotation.x > deg_to_rad(89):
+			$CameraPivot.rotation.x = clamp($CameraPivot.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+		
+		orthonormalize()
+		$CameraPivot.orthonormalize()
