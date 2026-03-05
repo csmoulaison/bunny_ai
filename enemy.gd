dd77fc6f1a5f32daa8bc065e13af3c4a2ff9f798
csmoulaison <csmoulaison@gmail.com>
2026-03-04 21:45:58 -0800

    Implement state machine based AI with patrol, investigate, and hunt behaviors responding to spatial sounds

--- enemy.gd
+++ enemy.gd
@@ -1,62 +1,204 @@
 extends CharacterBody3D
 
-enum BunnyChaseState { PATROL, DISTRACTED, INVESTIGATE, CHASE }
+# TODO(conner): Parameterize all the stuff that our indomitable designer will
+# want to tweak.
 
-# Bunny Chase Logic
-# Whenever an object which makes conspicuous noises makes a conspicuous noise,
-# it emits an "attract" signal, which is handled by our _on_attract function 
-# which keeps track of those signals.
-#
-# When an attraction is emitted, its position is checked against the existing
-# targets for whether it falls within a distance threshold. If it is within the
-# threshold, the score associated with the attractor is added to the matching
-# target and its position is updated to match the new attraction. If not, a new
-# target is created with the associated score.
-#
-# The target scores are constantly depreciating, so the most recent attractions
-# are more likely to be engaged with. Every tick, we see if any targets have a
-# high enough threshold to warrant a response, and our hare reacts accordingly.
-class Target:
+# NOTE(conner): For my own sanity, it might be worth making the data which is
+# associated with specific states vs global to all (or most) states explicitly
+# laid out into something like a class. Then the state specifics can 
+
+enum State { 
+	ROAM_MOVE, 
+	ROAM_LISTEN, 
+	INVESTIGATE_MOVE_TO_TARGET, 
+	INVESTIGATE_MOVE_IN_RADIUS, 
+	INVESTIGATE_LISTEN, 
+	HUNT 
+}
+
+class SearchRadiusPositionResult:
+	var success: bool
 	var position: Vector3
-	var score: float
+	func _init(suc: bool, pos: Vector3):
+		success = suc
+		position = pos
 
 @export var player: Node3D
 @export var path_nodes: Array[Node3D]
-@export var speed = 3.0
-@export var acceleration = 20.0
-@export var state: BunnyChaseState
+@export var speed: float = 3.0
+@export var acceleration: float = 20.0
+@export var state: State
+## Radii for detection zones (0: Death Zone, 1: Danger Zone, 2: Detection Zone)
+@export var detection_radii: Array[float] = [ 6.0, 12.0, 24.0 ]
 
 @onready var nav = $NavigationAgent3D
-@onready var current_node = 0
 
-var targets: Array[Target]
+var current_path_node: int = 0
+var current_target: Vector3
+
+## Used for both exiting and entering listen state
+var listen_countdown: float = randf() * 10.0
+var investigate_countdown: float = 0.0
 
 func _ready():
-	var attractors = get_tree().get_nodes_in_group("Attractors")
-	for attractor in attractors:
-		attractor.attract.connect(on_attract)
+	var sound_emitters = get_tree().get_nodes_in_group("SoundEmitters")
+	for emitter in sound_emitters:
+		emitter.sound.connect(on_sound)
+
+func on_sound(origin: Vector3, type: Sound.Type):
+	print("Sound triggered | pos: ", origin, " type: ", type)
 	
-func on_attract(pos: Vector3, score: float):
-	# TODO(conner): Calculate actual score based on distance and combine with
-	# previous targets as needed. Maybe add a third parameter for the "staying
-	# power" of the attraction, which is either averaged with or overwrites the
-	# previous value of the target, and controls how slow the cooldown effect is.
-	print("Attract | pos: ", pos, " score: ", score)
-	
+	# NOTE(conner): I think this detection behavior might be more coherent and
+	# organic if we fold it down into different score thresholds leading to 
+	# increasingly severe responses with the score being a function of sound
+	# "volume" and distance from EB.
+	#
+	# We can layer additional conditionals on top of this base to create bespoke
+	# behavior, but I think this would make things both clearer and more 
+	# dynamic. It's already being gestured at discretely as is, but with some
+	# situations that feel like gaps, like walking close in the detection zone.
+	var distance: float = (origin - position).length()
+	if distance < detection_radii[0]: # Death zone
+		if(type == Sound.Type.BREATH
+		|| type == Sound.Type.CROUCH_FOOTSTEP
+		|| type == Sound.Type.WALK_FOOTSTEP
+		|| type == Sound.Type.RUN_FOOTSTEP):
+			# TODO(conner): Kill the player
+			var _tmp
+	if distance < detection_radii[1]: # Danger zone
+		if(type == Sound.Type.WALK_FOOTSTEP
+		|| type == Sound.Type.RUN_FOOTSTEP):
+			state = State.HUNT
+	else: if distance < detection_radii[2]: # Detection zone
+		if(type == Sound.Type.RUN_FOOTSTEP):
+			current_target = origin
+			state = State.INVESTIGATE_MOVE_TO_TARGET
+			
 func _process(dt: float):
-	# TODO(conner): Figure out current target/state based on currently tracked
-	# targets. Represent all possible actions as utility functions, and add a 
-	# bias towards recently switch-to states to prevent schizophrenia.
-	var k = 0 # temp to prevent error
+	# TODO(conner): Change speed depending on state.
+	match state:
+		State.ROAM_MOVE:
+			roam_move_state(dt)
+		State.ROAM_LISTEN:
+			roam_listen_state(dt)
+		State.INVESTIGATE_MOVE_TO_TARGET:
+			investigate_move_to_target_state(dt)
+		State.INVESTIGATE_LISTEN:
+			investigate_listen_state(dt)
+		State.INVESTIGATE_MOVE_IN_RADIUS:
+			investigate_move_in_radius_state(dt)
+		State.HUNT:
+			hunt_state(dt)
+
+func roam_move_state(dt: float):
+	# Move towards the path nodes in sequence
+	if nav.is_target_reached():
+		current_path_node += 1
+		if current_path_node >= len(path_nodes):
+			current_path_node = 0
+	nav.target_position = path_nodes[current_path_node].position
+	# Randomly stop to listen
+	if(countdown_listen(dt)):
+		listen_countdown = randf() * 5.0
+		state = State.ROAM_LISTEN
+		
+func roam_listen_state(dt: float):
+	nav.target_position = position
+	if(countdown_listen(dt)):
+		listen_countdown = randf() * 10.0
+		state = State.ROAM_MOVE
+
+func investigate_move_to_target_state(_dt: float):
+	if (current_target - position).length() < 0.2:
+		nav.target_position = position
+		investigate_countdown = randf_range(5.0, 10.0)
+		listen_countdown = randf() * 4.0
+		state = State.INVESTIGATE_LISTEN
+	else:
+		nav.target_position = current_target
+	
+func investigate_listen_state(dt: float):
+	if(countdown_listen(dt)):
+		var count = 0
+		var space_state = get_world_3d().direct_space_state
+		var search_position_result: SearchRadiusPositionResult = try_find_search_radius_position(count, space_state)
+		while(!search_position_result.success):
+			count += 1
+			search_position_result = try_find_search_radius_position(count, space_state)
+		nav.target_position = search_position_result.position
+		state = State.INVESTIGATE_MOVE_IN_RADIUS
+	countdown_investigation_to_roam(dt)
+	
+func investigate_move_in_radius_state(dt: float):
+	if (nav.target_position - position).length() < 0.3:
+		nav.target_position = position
+		listen_countdown = randf() * 3.0
+		state = State.INVESTIGATE_LISTEN
+	countdown_investigation_to_roam(dt)
+	
+func hunt_state(_dt: float):
+	nav.target_position = player.position
 
 func _physics_process(dt: float):
-	nav.target_position = path_nodes[current_node].position
-	var cur_position = global_transform.origin
-	var next_position = nav.get_next_path_position()
-	velocity = velocity.move_toward((next_position - cur_position).normalized() * speed, acceleration * dt)
+	var target_velocity: Vector3 = Vector3.ZERO
+	var target_delta: Vector3 = nav.get_next_path_position() - global_transform.origin;
+	if(target_delta.length()) > 0.1:
+		target_velocity = target_delta.normalized() * speed
+	velocity = velocity.move_toward(target_velocity, acceleration * dt)
+	$Pivot.rotation.y = atan2(target_velocity.x, target_velocity.z)
 	move_and_slide()
+
+func try_find_search_radius_position(count: int, space_state: PhysicsDirectSpaceState3D) -> SearchRadiusPositionResult:
+	if count > 100: 
+		print("Too many attempts. Giving up finding new search radius position.")
+		return SearchRadiusPositionResult.new(true, position)
 	
-	if nav.is_target_reached():
-		current_node += 1
-		if current_node >= len(path_nodes):
-			current_node = 0
+	var angle: float = randf() * 2.0 * PI
+	var radius: float = sqrt(randf_range(2.0 - count / 50.0, 4.0 - count / 25.0))
+	var circle_position := current_target + Vector3(radius * cos(angle), 0.0, radius * sin(angle))
+	
+	# Check if position on circle hits a wall, defined as having
+	# an angle greater than 0.8 radians.
+	var ray_origin := Vector3(position.x, position.y + 0.0, position.z)
+	var ray_query := PhysicsRayQueryParameters3D.create(ray_origin, circle_position)
+	var result := space_state.intersect_ray(ray_query)
+	if !result.is_empty():
+		var normal: Vector3 = result["normal"]
+		var dot: float = normal.dot(Vector3.UP)
+		if dot >= 0.8 || dot <= -0.8:
+			print("Collided with wall!")
+			return SearchRadiusPositionResult.new(false, Vector3.ZERO)
+			
+	# Cast for closest y floor position
+	var closest_y: float = 1000.0
+	# UP from position
+	var ray_end := circle_position + (Vector3.UP * 10.0)
+	ray_query = PhysicsRayQueryParameters3D.create(circle_position, ray_end)
+	result = space_state.intersect_ray(ray_query)
+	if !result.is_empty():
+		closest_y = result["position"].y
+	# DOWN from position
+	ray_end = circle_position + (Vector3.DOWN * 10.0)
+	ray_query = PhysicsRayQueryParameters3D.create(circle_position, ray_end)
+	result = space_state.intersect_ray(ray_query)
+	if !result.is_empty():
+		var down_y = result["position"].y
+		if abs(down_y - position.y) < abs(closest_y - position.y):
+			closest_y = down_y
+		
+	if nav.is_target_reachable():
+		return SearchRadiusPositionResult.new(true, Vector3(circle_position.x, closest_y, circle_position.z))
+	print("target not reachable! ", count, nav.target_position)
+	return SearchRadiusPositionResult.new(false, Vector3.ZERO)
+	
+func countdown_investigation_to_roam(dt: float):
+	investigate_countdown -= dt
+	if investigate_countdown < 0.0:
+		# TODO(conner): Check for closest path node to move to. Even better 
+		# would be get whichever node is furthest "forward" on the path from
+		# where we are. Some dot product stuff, you know.
+		state = State.ROAM_MOVE
+		
+func countdown_listen(dt: float) -> bool:
+	listen_countdown -= dt
+	return listen_countdown < 0.0
--- player.gd
+++ player.gd
@@ -1,15 +1,33 @@
 extends CharacterBody3D
 
-signal attract(pos: Vector3, score: float)
+signal sound(pos: Vector3, type: Sound.Type)
 
-@export var speed = 10.0
+enum MoveState {
+	WALK,
+	CROUCH,
+	RUN
+}
+
+@export var crouch_speed = 1.5
+@export var walk_speed = 4.0
+@export var run_speed = 10.0
+
+@export var crouch_height = 0.75
+@export var normal_height = 1.5
+
+@export var crouch_footstep_interval = 1.0
+@export var walk_footstep_interval = 0.6
+@export var run_footstep_interval = 0.3
+
 @export var acceleration = 50.0
 @export var fall_acceleration = 75.0
 @export var mouse_sensitivity = 0.01
-@export var footsteps_per_second = 0.5
 
+@onready var camera: Node3D = $CameraPivot
+
+var move_state: MoveState
 var target_velocity = Vector3.ZERO
-var footstep_cooldown = footsteps_per_second * 0.5
+var footstep_cooldown = 0.0
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -26,24 +44,57 @@ func _physics_process(dt: float):
 	if Input.is_action_pressed("move_forward"):
 		input.y -= 1.0
 		
+	if Input.is_action_pressed("crouch"):
+		move_state = MoveState.CROUCH
+	else: if Input.is_action_pressed("run"):
+		move_state = MoveState.RUN
+	else:
+		move_state = MoveState.WALK
+	
+	# Set state specific values
+	var footstep_sound: Sound.Type
+	var current_speed: float
+	var cam_target_height = normal_height
+	var footstep_interval: float
+	var footstep_volume_db: float
+	match move_state:
+		MoveState.CROUCH:
+			footstep_sound = Sound.Type.CROUCH_FOOTSTEP
+			current_speed = crouch_speed
+			cam_target_height = crouch_height
+			footstep_interval = crouch_footstep_interval
+			footstep_volume_db = -30.0
+		MoveState.WALK:
+			footstep_sound = Sound.Type.WALK_FOOTSTEP
+			current_speed = walk_speed
+			footstep_interval = walk_footstep_interval
+			footstep_volume_db = -15.0
+		MoveState.RUN:
+			footstep_sound = Sound.Type.RUN_FOOTSTEP
+			current_speed = run_speed
+			footstep_interval = run_footstep_interval
+			footstep_volume_db = 0.0
+		MoveState.WALK, MoveState.RUN:
+			cam_target_height = normal_height
+			
+	camera.position.y = move_toward(camera.position.y, cam_target_height, dt * 6.0)
+	
 	if input != Vector2.ZERO:
 		input = input.normalized()
 		footstep_cooldown -= dt
 		if(footstep_cooldown < 0.0):
-			footstep_cooldown = footsteps_per_second
+			footstep_cooldown = footstep_interval
+			$FootstepSound.volume_db = footstep_volume_db
 			$FootstepSound.play()
-			emit_signal("attract", position, 5.0)
+			emit_signal("sound", position, footstep_sound)
 	else:
-		footstep_cooldown = footsteps_per_second * 0.5
+		footstep_cooldown = 0.0
 			
 	var direction = (transform.basis * Vector3(input.x, 0.0, input.y))
-		
-	target_velocity.x = move_toward(target_velocity.x, direction.x * speed, acceleration * dt)
-	target_velocity.z = move_toward(target_velocity.z, direction.z * speed, acceleration * dt)
-	
+	target_velocity.x = move_toward(target_velocity.x, direction.x * current_speed, acceleration * dt)
+	target_velocity.z = move_toward(target_velocity.z, direction.z * current_speed, acceleration * dt)
 	if not is_on_floor():
 		velocity.y = velocity.y - (fall_acceleration * dt)
-		
 	velocity.x = target_velocity.x
 	velocity.z = target_velocity.z
 	move_and_slide()
